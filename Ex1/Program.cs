7e989501c7be68cb75547ef35a1dc0e54b733a22
Maxim Khamidulin <Maxim Khamidulin@DESKTOP-O9G6VIU>
2024-10-20 18:43:14 +0300

    Решена задача №2

--- Ex1/Program.cs
+++ Ex1/Program.cs
@@ -10,6 +10,17 @@ namespace Ex1
     {
         static void Main(string[] args)
         {
+            int[] array = new int[7];
+            int count = 7;
+            float Sum = 0;
+            for (int i = 0; i < 7; i++)
+            {
+                array[i] = Convert.ToInt32(Console.ReadLine());
+                Sum += array[i];
+            }
+
+            System.Console.WriteLine("Среднее значение {0}", Sum / count);
+            System.Console.ReadKey();
         }
     }
 }
---
+++ Ex2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[15];
+            Random random = new Random();
+
+            for (int i = 0; i < 15; i++)
+            {
+                array[i] = random.Next(0, 50);
+            }
+
+            int array_max = array[0];
+            int array_min = array[0];
+
+            foreach (int a in array)
+            {
+                if (a > array_max)
+                {
+                    array_max = a;
+                }
+                if (a < array_min)
+                {
+                    array_min = a;
+                }
+            }
+
+            for (int i = 0; i < 15; i++)
+            {
+                Console.Write("{0, 3}", array[i]);
+
+            }
+            Console.WriteLine();
+            Console.WriteLine("Максимальное значение {0}", array_max);
+            Console.WriteLine("Минимальное значение {0}", array_min);
+            Console.ReadKey();
+        }
+    }
+}
