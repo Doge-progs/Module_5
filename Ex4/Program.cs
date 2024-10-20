4f3aae50a60ba8af87562ff490df567ab8825131
Maxim Khamidulin <Maxim Khamidulin@DESKTOP-O9G6VIU>
2024-10-20 18:53:35 +0300

    Решена задача №4

---
+++ Ex4/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[20];
+            Random random = new Random();
+
+            for (int i = 0; i < 20; i++)
+            {
+                array[i] = random.Next(-50, 50);
+            }
+
+            int counter = 0;
+
+            for (int i = 0; i < 20; i++)
+            {
+                if ((array[i] > 0) && (array[i] % 2 != 0) && (i % 2 != 0))
+                {
+                    counter++;
+                }
+            }
+            for (int i = 0; i < 20; i++)
+            {
+                System.Console.Write("{0, 3}", array[i]);
+            }
+            System.Console.WriteLine();
+            System.Console.WriteLine("Количество положительных нечетных чисел стоящих на четных местах = {0}", counter);
+            System.Console.ReadKey();
+        }
+    }
+}
